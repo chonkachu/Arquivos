26c0cb9e0f6a16dc467d25ac3ce9303270cea43e
TaresuSenu <tsdq2005@gmail.com>
2024-04-07 21:09:52 -0300

    receba

--- include/db.c
+++ include/db.c
@@ -1,13 +1,15 @@
 #include "db.h"
 
 int create_table(FILE *csv_file, FILE *bin_file){
+    data_registry *a=(data_registry*)malloc(sizeof(data_registry));
+    data_registry **table=(data_registry**)malloc(sizeof(data_registry*));
     while(1){
         char str[300];
         int rotation=0;
         for(int i=0;;i++){
             char a;
             if(a=getc(csv_file)){
-                if(a==','){
+                if(a==',' || a=='\n'){
                     str[i]='\0';
                     break;
                 }else{
@@ -16,21 +18,54 @@ int create_table(FILE *csv_file, FILE *bin_file){
             }
         }
         if(rotation==0){
-            if(str[0]!='\0'){
-                int x=-1;
+            if(str[0]=='\0'){
+                a->id = -1;
+            }else{
+                a->id = atoi(str);
             }
-           int x = atoi(str);
         }else if(rotation==1){
-           int x = atoi(str);
+            if(str[0]=='\0'){
+                a->idade = -1;
+            }else{
+                a->idade = atoi(str);
+            }
         }else if(rotation==2){
+            a->tamNomeJog = strlen(str);
+            if(str[0]=='\0'){
+                a->nomeJogador = '$';
+            }else{
+                a->nomeJogador = str;
+            }
 
         }else if(rotation==3){
-
+            a->tamNacionalidade = strlen(str);
+            if(str[0]=='\0'){
+                a->nacionalidade = '$';
+            }else{
+                a->nacionalidade = str;
+            }
         }else{
-
+             a->tamNomeClube = strlen(str);
+            if(str[0]=='\0'){
+                a->nomeClube = '$';
+            }else{
+                a->nomeClube = str;
+            }
         }
+
         rotation++;
-        if(rotation==5) rotation=0;
+
+        if(rotation==5){
+             rotation=0;
+             a->prox;
+             a->removido=0;
+             a->tamanhoRegistro;
+            
+            // maloca nova posiçao no vetor d eponteiro e mete o ponteiro pra a
+
+            
+             a=(data_registry*)malloc(sizeof(data_registry));
+        }
     }
 }
 
